8703c526b6cbf6e76eaa3c283566e24576bc41c5
njleonzhang <scream3616@sina.com>
2017-11-17 14:33:33 +0800

    release

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/element-ui-pro/',
 
     /**
      * Source Maps
