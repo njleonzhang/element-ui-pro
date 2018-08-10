078f9390152362647762320af7849d172ab124ee
njleonzhang <scream3616@sina.com>
2018-08-10 16:06:38 +0800

    make post css work

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -104,6 +104,7 @@ module.exports = {
         use: [
           'vue-style-loader',
           'css-loader',
+          'postcss-loader',
           'sass-loader',
           {
             loader: 'sass-resources-loader',
